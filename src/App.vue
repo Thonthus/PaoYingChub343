f0ec7d78b45340814749055b3919b524a57de646
Thonthus <thonthus2546@gmail.com>
2023-09-07 01:56:38 +0700

    setup

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+</script>
+
+<template>
+  efefefe
+  <h1>Rock, Paper, Scissors</h1>
+    <div class="game-container">
+        <div class="choice" id="rock">
+            <img src="rock.png" alt="Rock" width="100">
+            <p>Rock</p>
+        </div>
+        <div class="choice" id="paper">
+            <img src="paper.png" alt="Paper" width="100">
+            <p>Paper</p>
+        </div>
+        <div class="choice" id="scissors">
+            <img src="scissors.png" alt="Scissors" width="100">
+            <p>Scissors</p>
+        </div>
+    </div>
+    <div class="result" id="result"></div>
+</template>
+
+<style scoped>
+ 
+</style>
